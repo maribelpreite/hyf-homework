b73fba6eaef6036192dde66e12157e1abc354f88
Maribel Preite <maribelpreite@gmail.com>
2024-05-05 10:42:45 +0200

    Implemented feedback PART 3

--- javascript/javascript1/week2/student_manager.js
+++ javascript/javascript1/week2/student_manager.js
@@ -1,4 +1,9 @@
 const class07Students = ["Anna", "Jonas", "Micah", "Noah", "Louise", "Jonathan", "Michael"];
+
+function maxNumberOfStudents () {
+    return class07Students.splice(6, class07Students.length -1);
+}
+
 function addStudentToClass(studentName) {
     if (class07Students.length>5 && studentName !== "Queen Margrethe") {
         console.log(`Cannot add more students to class 07`);
@@ -11,10 +16,12 @@ function addStudentToClass(studentName) {
     }
 }
 
-function getNumberOfStudents(class07Students) {
+function getNumberOfStudents() {
     console.log(`There are ${class07Students.length} in class 07. See the full list of students below:`);
     console.log(class07Students);
 }
 
+maxNumberOfStudents();
 addStudentToClass("Micah");
-getNumberOfStudents(class07Students);
+addStudentToClass("Queen Margrethe");
+getNumberOfStudents();
