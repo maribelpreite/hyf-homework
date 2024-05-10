20796c6097eed54f81db73cd90f3f416b1adf815
Maribel Preite <maribelpreite@gmail.com>
2024-05-11 01:03:49 +0200

    Edited Task 2

--- javascript/javascript1/week3/time_calculator.js
+++ javascript/javascript1/week3/time_calculator.js
@@ -1,11 +1,11 @@
 // DATA
-const travelInformation = {
+const travelData = {
     speed: 50,
     destinationDistance: 432,
   };
 
   // LOGIC
-function estimatedTime(speed, destinationDistance) {
+function estimatedTime(travelInformation, speed, destinationDistance) {
     const timeFloat = travelInformation["destinationDistance"]/travelInformation["speed"];
     const hour = Math.floor(timeFloat);
     const minutesFloat = (timeFloat - hour) * 60;
@@ -17,8 +17,8 @@ function estimatedTime(speed, destinationDistance) {
  function present (time) {
   const hourString = time.hour > 1 ? `${time.hour} hours` : `${time.hour} hour`;
   const minuteString = time.minutes > 1 ? `${time.minutes} minutes` : `${time.minutes} minute`;
-  console.log(`${hourString} and ${minuteString}`)
+  console.log(`It will take you ${hourString} and ${minuteString} to arrive at your destination.`)
 } 
 
-  const travelTime = estimatedTime(travelInformation);
+  const travelTime = estimatedTime(travelData);
   present(travelTime); 
