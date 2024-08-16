4e8ca1de7f01cd1e5c67a1d2a5ab1d334997a3a2
Maribel Preite <maribelpreite@gmail.com>
2024-08-16 19:53:40 +0200

    get /search route

--- nodejs/week2/index.js
+++ nodejs/week2/index.js
@@ -1,13 +1,47 @@
-import express from 'express'
+import express from 'express';
+import { readFile } from 'node:fs/promises';
+
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Support parsing JSON requests
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("This is a search engine");
+async function readJSONFile (filePath) {
+    try {
+        const data = await readFile(filePath, 'utf8');
+        return JSON.parse(data);
+    } catch (error) {
+        console.error('Something went wrong while trying to read JSON file:', error);
+        throw error;
+    }
+}
+
+app.get("/search", async (req, res) => {
+    try {
+        const document = await readJSONFile('./documents.json');
+        const query = req.query.q;
+
+        if (query) {
+            const filteredSearch = document.filter(obj => {
+                return Object.values(obj).some(value => {
+                    if (typeof value === 'string') {
+                        return value.toLowerCase().includes(query.toLowerCase());
+                    } 
+                    return false;
+                })
+            })
+            res.json(filteredSearch);
+
+        } else {
+            res.json(document);
+        }
+
+    } catch (error) {
+        console.error('Something went wrong while trying to search for your document:', error)
+        res.status(500).json({error: 'Error while processing your request.'})
+    }
 });
+
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
