13834c374a894023e19cc851c202fb4e60cd72a6
Maribel Preite <maribelpreite@gmail.com>
2024-08-17 19:46:54 +0200

    post /search route tested with postman :)

--- nodejs/week2/src/routers/searchRouter.js
+++ nodejs/week2/src/routers/searchRouter.js
@@ -12,7 +12,10 @@ searchRouter.get("/", async (req, res) => {
     if (query) {
       filteredSearch = document.filter((obj) => {
         return Object.values(obj).some((value) => {
-            return typeof value === 'string' && value.toLowerCase().includes(query.toLowerCase());
+          return (
+            typeof value === "string" &&
+            value.toLowerCase().includes(query.toLowerCase())
+          );
         });
       });
     } else {
@@ -24,6 +27,47 @@ searchRouter.get("/", async (req, res) => {
       "Something went wrong while trying to search for your document:",
       error,
     );
+    res.status(500).json({ error: "Error while processing your request." });
+  }
+});
+
+searchRouter.post("/", async (req, res) => {
+  const query = req.query.q;
+  const fields = req.body.fields;
+
+  if (query && fields) {
+    return res
+      .status(400)
+      .json({
+        error:
+          "Both the query and the request body cannot be provided together.",
+      });
+  }
+
+  try {
+    const document = await readJSONFile("./src/documents.json");
+    let filteredSearch = [];
+
+    if (query) {
+      filteredSearch = document.filter((obj) =>
+        Object.values(obj).some(
+          (value) =>
+            typeof value === "string" &&
+            value.toLowerCase().includes(query.toLowerCase()),
+        ),
+      );
+    } else if (fields) {
+      filteredSearch = document.filter((obj) =>
+        Object.entries(fields).every(
+          ([key, value]) => String(obj[key]) === String(value),
+        ),
+      );
+    } else {
+      filteredSearch = document;
+    }
+    res.json(filteredSearch);
+  } catch (error) {
+    console.error("Something went wrong with the server:", error);
     res.status(500).json({ error: "Error while processing your request." });
   }
 });
